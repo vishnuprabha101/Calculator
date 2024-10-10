828fb46bb780feb9c4748b00982115d6a57df070
vishnuprabha <vishnuprabha101@gmail.com>
2024-10-10 17:38:53 -0400

    Final commit

--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315d25502c122b34f288b9a1ae15c38d45e54c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08684c61a6e9171c4ca99b67fe2bf70536df1502")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
